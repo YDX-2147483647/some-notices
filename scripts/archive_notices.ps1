41ceaa4e1566af4dd37ece3eabe6a9169ff3a8b8
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-10-17 16:30:48 +0800

    Fix `archive_notices.py` regard `.gitkeep` as a notice
    - feature: dry-run, better assertion error.
    - internal: `str` → `Path`.

--- scripts/archive_notices.ps1
+++ scripts/archive_notices.ps1
@@ -1,2 +1,2 @@
-.\scripts\env\Scripts\activate
+.\scripts\env\Scripts\Activate.ps1
 python "./scripts/archive_notices.py" "./_notices/" --verbose
--- scripts/archive_notices.py
+++ scripts/archive_notices.py
@@ -1,31 +1,34 @@
 from argparse import ArgumentParser
 from datetime import date, timedelta
-from os import listdir
-from os.path import join
+from pathlib import Path
 
-from frontmatter import parse, load, dump
+from frontmatter import dump, load, parse
 
 
-def get_outdated_notices(root: str, before: date) -> list[str]:
+def get_outdated_notices(root: Path, before: date) -> list[Path]:
     """ Get outdated but active notices
     :returns: list of filepaths.
     """
 
-    outdated: list[str] = []
+    outdated: list[Path] = []
 
-    for filename in listdir(root):
-        filepath = join(root, filename)
-        with open(filepath, encoding='utf-8') as f:
+    for filepath in root.glob('*.md'):
+        with filepath.open(mode='r', encoding='utf-8') as f:
             metadata, _ = parse(f.read())
 
-        assert 'status' in metadata
+        assert 'status' in metadata, f'No `status` in “{filepath}”.'
         if metadata['status'] == 'active' and 'due' in metadata:
             due: date | int | float = metadata['due']
+            assert isinstance(due, date | int | float), \
+                f'Cannot parse `due`: “{due}” in “{filepath}”.'
+
+            # Unify `due` to a `date`
             if isinstance(due, date):
                 pass
             else:
                 last_update: date = max(metadata['updated_on'])
-                assert isinstance(last_update, date)
+                assert isinstance(last_update, date),\
+                    f'The item in `updated_on` is not a date: “{last_update}” in “{filepath}”.'
                 due = last_update + timedelta(days=due)
 
             if due < before:
@@ -34,22 +37,26 @@ def get_outdated_notices(root: str, before: date) -> list[str]:
     return outdated
 
 
-def set_as_archived(filepath: str) -> None:
+def set_as_archived(filepath: Path) -> None:
     post = load(filepath)
     post.metadata['status'] = 'archived'
     dump(post, filepath)
 
 
-def prepare_parser() -> ArgumentParser:
+def build_parser() -> ArgumentParser:
     parser = ArgumentParser(
         description='Filter out outdated notices and archive them.')
     parser.add_argument(
         'root',
-        type=str,
+        type=Path,
         help='The root folder of the notices.'
     )
     parser.add_argument(
         '--verbose',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--dry-run',
         action='store_true'
     )
 
@@ -57,8 +64,7 @@ def prepare_parser() -> ArgumentParser:
 
 
 if __name__ == '__main__':
-    parser = prepare_parser()
-    args = parser.parse_args()
+    args = build_parser().parse_args()
 
     outdated = get_outdated_notices(args.root, date.today())
 
@@ -72,7 +78,11 @@ if __name__ == '__main__':
     for notice in outdated:
         if args.verbose:
             print(f"  - {notice}")
-        set_as_archived(notice)
+        if not args.dry_run:
+            set_as_archived(notice)
 
     if args.verbose and outdated:
         print('✓ Done.')
+
+    if args.dry_run:
+        print('—— Dry run. (nothing changed)')
