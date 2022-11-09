f4ed2754a17c9684f49fbffc6eea8c28ee6a39b2
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2022-11-09 10:53:33 +0800

    Update `deploy.ps1`

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -5,13 +5,13 @@ $env:JEKYLL_ENV = 'production'
 bundler exec jekyll build
 
 Write-Host 'Syncing files …'
-rsync --archive --checksum --human-readable --delete _site/ SomeNotices:/var/www/html/ --info=progress2
+rsync --recursive --checksum --human-readable --delete _site/ SomeNotices:/var/www/html/ --info=progress2
 # For those who cannot rsync:
 # scp 只会用新文件覆盖旧文件，不会单独删除旧文件。
 # ssh SomeNotices rm -rf /var/www/html/*
 # scp -r ./_site/* SomeNotices:/var/www/html/
 
-Write-Host 'Reloading the server …'
-ssh SomeNotices nginx -s reload
+# Write-Host 'Reloading the server …'
+# ssh SomeNotices sudo -t nginx -s reload
 
 Write-Host '✓ Done.'
